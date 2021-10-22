9b9bac8680d794080ffc4900c3a1282fbae15540
jyf111 <1340549313@qq.com>
2021-10-22 16:25:53 +0800

    add exact matches

--- myweb/views.py
+++ myweb/views.py
@@ -82,4 +82,29 @@ def author(request, pid):
     return render(request, 'author.html', {'author': author})
 
 def article(request, doi):
-    return render(request, 'article.html', {'article': utils.semantic.Article(doi)})
+    article = utils.semantic.Article(doi)
+    from pyecharts import options as opts
+    from pyecharts.charts import Bar
+    citcnt = {}
+    mnyear, mxyear = time.localtime(time.time()).tm_year, time.localtime(time.time()).tm_year
+    for citation in article.citations:
+        year = citation['year']
+        if year<mnyear:
+            mnyear = year
+        if year>mxyear:
+            mxyear = year
+        if year not in citcnt:
+            citcnt[year] = 1
+        else:
+            citcnt[year] += 1
+    for i in range(mnyear, mxyear+1):
+        if i not in citcnt:
+            citcnt[i] = 0
+    bar = (
+        Bar(init_opts=opts.InitOpts(width="300px", height="200px"))
+        .add_xaxis(list(range(mnyear, mxyear+1)))
+        .add_yaxis("count", [citcnt[year] for year in range(mnyear, mxyear+1)], itemstyle_opts = opts.ItemStyleOpts(color="blue"))
+        .set_global_opts(title_opts=opts.TitleOpts(title="citations"))
+        .render('static/citation.html')
+    )
+    return render(request, 'article.html', {'article': article})
